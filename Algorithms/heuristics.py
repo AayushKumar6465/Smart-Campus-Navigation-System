96cce288ec07b3118ce6e83f05f4f8bd267ff4be
AayushKumar6465 <aayushkumar9376@gmail.com>
2025-11-04 23:32:52 +0530

    Making file structures and creating a heiristic file

---
+++ Algorithms/heuristics.py
@@ -0,0 +1,11 @@
+import math
+
+def euclidean_distance(node1, node2, nodes):
+    x1, y1 = nodes[node1]["x"], nodes[node1]["y"]
+    x2, y2 = nodes[node2]["x"], nodes[node2]["y"]
+    return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+
+def manhattan_distance(node1, node2, nodes):
+    x1, y1 = nodes[node1]["x"], nodes[node1]["y"]
+    x2, y2 = nodes[node2]["x"], nodes[node2]["y"]
+    return abs(x1 - x2) + abs(y1 - y2)
